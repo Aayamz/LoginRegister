ee5c4f5393e38f3fe20fd10c43396a0757c9caa3
Aayam <aayamsss@gmail.com>
2016-06-11 08:02:59 -0700

    Variables Modified..

---
+++ AndroidStudioProjects/LoginRegister/app/src/main/java/com/conceptiveideas/aayam/loginregister/RegisterActivity.java
@@ -0,0 +1,28 @@
+package com.conceptiveideas.aayam.loginregister;
+
+import android.preference.EditTextPreference;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //setting up Variables
+        final EditText edAge;
+        final EditText edName;
+        final EditText edUsername;
+        final EditText edPassword;
+        final Button bRegBtn;
+
+
+
+    }
+
+}
